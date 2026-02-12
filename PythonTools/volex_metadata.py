79ba5ae126c398bbf99de6bcacc11423c48bfdaa
Fadi Abo Hadba <fadi.hadbh28@gmail.com>
2026-02-12 14:52:13 +0100

    Update volex_metadata.py

--- PythonTools/volex_metadata.py
+++ PythonTools/volex_metadata.py
@@ -1,18 +1,3 @@
-# FRAUNHOFER IIS CONFIDENTIAL
-# __________________
-#
-# Fraunhofer IIS
-# Copyright (c) 2016-2021
-# All Rights Reserved.
-#
-# This file is part of the PythonTools project.
-#
-# NOTICE:  All information contained herein is, and remains the property of Fraunhofer IIS and its suppliers, if any.
-# The intellectual and technical concepts contained herein are proprietary to Fraunhofer IIS and its suppliers and may
-# be covered by German and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
-# permission is obtained from Fraunhofer IIS.
-
 
 from __future__ import annotations
 
